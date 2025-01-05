14e7005d44438434fdfe137676df0674e0e7c02c
KUNAL CHOURE <chourekunal4@gmail.com>
2025-01-05 09:52:37 +0530

    hero section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-
+import Hero from "@/components/hero";
 export default function Home() {
   return (
     <>
      <main className="min-h-screen bg-black/[0.9] antialiased bg-grid-white/[0.02]">
-
+      <Hero/>
      </main>
     </>
   );
